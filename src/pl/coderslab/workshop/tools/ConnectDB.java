bc50255006f0bcea4d834423ff60201a3395822e
krzysztofcz <34889794+krzysztofcz@users.noreply.github.com>
2018-02-12 16:33:10 +0100

    update

--- src/pl/coderslab/workshop/tools/ConnectDB.java
+++ src/pl/coderslab/workshop/tools/ConnectDB.java
@@ -3,13 +3,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class connectDB {	
-
-	Connection con = connect(); 
+public class ConnectDB {	
 	
 	public static Connection connect() {
 		Connection con = null;
-		String url="jdbc:mysql://127.0.0.1:3306/workshop";
+		String url="jdbc:mysql://127.0.0.1:3306/";
 		String db="workshop";
 		String options="?autoReconnect=true&useSSL=false";
 		String user="root";
