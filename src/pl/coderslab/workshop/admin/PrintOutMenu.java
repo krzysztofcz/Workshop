ce7de437fdd9a95d5fa1ede0d116dc3b5651387e
krzysztofcz <34889794+krzysztofcz@users.noreply.github.com>
2018-02-12 20:43:14 +0100

    clasa ktora bedzie drukowac menu na ekranie

--- src/pl/coderslab/workshop/admin/PrintOutMenu.java
+++ src/pl/coderslab/workshop/admin/PrintOutMenu.java
@@ -2,16 +2,34 @@ package pl.coderslab.workshop.admin;
 
 public class PrintOutMenu {
 
+
+	
+//	/**
+//	 * @param menuName
+//	 * @return 
+//	 */
+//	public PrintOutMenu(String menuName) {
+//		this.menuName = menuName;
+//		System.out.print("=== START MENU ===\n");
+//		System.out.print("Wybierz jedną z opcji ( wielkosc liter przy komednach nie ma znaczenia ): \n");
+//		System.out.print("1. add - dodanie "+this.menuName+"\n");
+//		System.out.print("2. edit- edycja "+this.menuName+"\n");
+//		System.out.print("3. delete- edycja "+this.menuName+"\n");
+//		System.out.print("4. QUIT- zakończenie programu\n");
+//		System.out.print("=== KONIEC MENU ===\n");
+//		return;
+//	}
+	
 	/**
 	 * @param menuName
+	 * 
 	 */
 	public PrintOutMenu(String menuName) {
 		this.menuName = menuName;
 	}
 
 
-	private String menuName;
-	
+	private String menuName;	
 	
 	/**
 	 * @return the menuName
@@ -35,11 +53,11 @@ public class PrintOutMenu {
 	public String toString() {
 		StringBuffer sb= new StringBuffer();
 		sb.append("=== START MENU ===\n");
-		sb.append("Wybierz jedną z opcji : \n");
-		sb.append("add - dodanie "+this.menuName+"\n");
-		sb.append("edit - edycja "+this.menuName+"\n");
-		sb.append("delete - edycja "+this.menuName+"\n");
-		sb.append("quit - zakończenie programu\n");
+		sb.append("Wybierz jedną z opcji ( wielkosc liter przy komednach nie ma znaczenia ): \n");
+		sb.append("1. add - dodanie "+this.menuName+"\n");
+		sb.append("2. edit- edycja "+this.menuName+"\n");
+		sb.append("3. delete- edycja "+this.menuName+"\n");
+		sb.append("4. QUIT- zakończenie programu\n");
 		sb.append("=== KONIEC MENU ===");
 		return sb.toString();
 	}
