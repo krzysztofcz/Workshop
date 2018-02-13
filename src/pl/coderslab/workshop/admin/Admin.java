c31acec51f46b0a58855f053c5f7d83e55a5e3dc
krzysztofcz <34889794+krzysztofcz@users.noreply.github.com>
2018-02-13 15:17:59 +0100

    new class Admin to help some thing in admin user exercise group and soultions

---
+++ src/pl/coderslab/workshop/admin/Admin.java
@@ -0,0 +1,13 @@
+package pl.coderslab.workshop.admin;
+
+public class Admin {
+	
+	/**zwraca true of false jesli zmienna(tutaj string) jest pusta 
+	 * @param string - Zmienna napis
+	 * @return
+	 */
+	public static boolean checkNull(String str){
+		return str.equals(null); 
+	}
+	
+}
